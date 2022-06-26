f35bce67cd3ab4c5a9e38e841fc6c08442c58f90
MR. MIND <104621741+MIND163-XD@users.noreply.github.com>
2022-06-26 13:10:41 +0600

    Create BD-8DIGIT.py

---
+++ BD-8DIGIT.py
@@ -0,0 +1,34 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from fire64 import Subscraption
+ 
+        Subscraption()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from fire32 import Subscraption
+ 
+ 
+        Subscraption()
+ 
+ 
+ 
+ 
